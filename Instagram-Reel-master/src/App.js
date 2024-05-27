a4f5fb4b5944c51dab487b614ec1908229d20f60
Ashish jangid <141478890+ashishjangid45@users.noreply.github.com>
2024-05-27 10:08:01 +0530

    first commit

---
+++ Instagram-Reel-master/src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+import Video from "./components/Video/Video";
+
+import Logo from "./img/icon.png";
+
+import Vid1 from "./videos/video1.MP4";
+import Vid2 from "./videos/video2.MP4";
+import Vid3 from "./videos/video3.MP4";
+
+import "./App.css";
+
+export default function App() {
+  const data = [
+    {
+      channel: "aaa",
+      song: "song-1",
+      url: Vid1,
+      likes: "32",
+      comment: "2",
+      shares: "23",
+    },
+    {
+      channel: "bbb",
+      song: "song-2",
+      url: Vid2,
+      likes: "3",
+      comment: "22",
+      shares: "23",
+    },
+    {
+      channel: "ccc",
+      song: "song-3",
+      url: Vid3,
+      likes: "89",
+      comment: "23",
+      shares: "29",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <center>
+        <div className="logo">
+          <img alt="logo" src={Logo} className="insta-logo" />
+        </div>
+        <h3>Reel</h3>
+        {/*  */}
+
+        <div className="video-container" id="video-container">
+          {/*  */}
+
+          {data.map((list, i) => (
+            <Video
+              key={i}
+              channel={list.channel}
+              song={list.song}
+              url={list.url}
+              likes={list.likes}
+              comment={list.comment}
+              shares={list.shares}
+            />
+          ))}
+
+          {/*  */}
+        </div>
+      </center>
+    </div>
+  );
+}
